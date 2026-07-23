a74e82a5064cbd8804b9bea38eb8e2f5953e2984
MC J <jmc8@qq.com>
2026-07-23 14:50:01 +0800

    增加一些宏定义启动前端渲染

--- include/lc/prelude.hpp
+++ include/lc/prelude.hpp
@@ -1,5 +1,16 @@
 #pragma once
 
+// 不知道为啥Resharper的__cpp_concepts值很低，导致expected之类的会报错，手动指定一下
+#if defined(__RESHARPER__) && __cpp_concepts < 202002L
+#  undef __cpp_concepts
+#  define __cpp_concepts 202002L
+#endif
+
+#if defined(__RESHARPER__) && !defined(__cpp_lib_contracts)
+#  define __cpp_lib_contracts
+#endif
+
+#include <iostream>
 #include <algorithm>
 #include <array>
 #include <charconv>
